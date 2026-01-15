6819b362102c9e2abcb7af79bee143bf6e8870d7
Saqib Saleem <saqibsaleem173@gmail.com>
2026-01-15 01:06:27 +0000

    Arrays in typeScript

---
+++ Arrays.ts
@@ -0,0 +1,7 @@
+let NamesArray: string[] = ["Haider", "John", "Sidhu"];
+let AgesArray: number[] = [25, 30, 35];
+let MixedArray: (string | number)[] = ["John", 25, "Wick", 30];
+let StudentArray:Array<string> = ["Saqib", "Ahmed", "Ali"];
+
+NamesArray.push("Bilal")
+console.log(NamesArray);
