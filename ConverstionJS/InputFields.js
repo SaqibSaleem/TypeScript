72cdfd4563f90b040fc350f9afaa0a87622ed415
Saqib Saleem <saqibsaleem173@gmail.com>
2026-01-15 00:31:24 +0000

    TypeScript

---
+++ ConverstionJS/InputFields.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// function to get the values from input fields
+function GetResult() {
+    var GetName = $("#txtName").val();
+    var GetPassword = $("#txtPassword").val();
+    var GetAge = $("#txtAge").val();
+}
---
+++ InputFields.ts
@@ -0,0 +1,6 @@
+// function to get the values from input fields
+function GetResult() {
+    var GetName = $("#txtName").val();
+    var GetPassword = $("#txtPassword").val();
+    var GetAge = $("#txtAge").val();
+}
